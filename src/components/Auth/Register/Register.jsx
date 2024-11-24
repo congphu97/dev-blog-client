da59acf151911f40af7193c10f7bf7db6eef9d5d
quocanh-dev <quocanhdev99@gmail.com>
2024-11-24 22:50:54 +0700

    feature(dev-blog)(blog): Remove dead code

--- src/components/Auth/Register/Register.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Register.scss";
-
-const RegisterComponent = () => {
-  return <div className="register-container">Register</div>;
-};
-
-export default RegisterComponent;
