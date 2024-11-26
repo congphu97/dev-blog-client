ca9cf7859f726855ec37f7ac8aac876748d585a8
quocanh-dev <quocanhdev99@gmail.com>
2024-11-26 23:06:52 +0700

    feature(dev-blog)(blog): Update Login UI

--- src/pages/Auth/Login/Login.jsx
+++ src/pages/Auth/Login/Login.jsx
@@ -1,28 +1,28 @@
 import React from "react";
+import { FaGithub, FaGoogle } from "react-icons/fa";
 import "./Login.scss";
 
 const LoginComponent = () => {
   return (
     <div className="login-container">
-      <form>
-        <div>
-          <label>Email:</label>
-          <input
-            type="email"
-            // onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            // onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit"></button>
-      </form>
+      <div class="login-logo">
+        <img
+          className="login-logo-img"
+          src="/assets/images/logo-dark-mode.png"
+          alt="logo-dark-mode"
+        />
+      </div>
+      <div className="login-btn-groups">
+        <button className="login-btn">
+          <FaGoogle className="social-icon" /> Google
+        </button>
+        <button className="login-btn">
+          <FaGithub className="social-icon" /> GitHub
+        </button>
+      </div>
+      <span className="login-txt">
+        Log in with your account to find out more!
+      </span>
     </div>
   );
 };
