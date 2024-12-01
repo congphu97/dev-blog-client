bfc203dc82c6e0d1fa8766ba623c05130f008e05
quocanh-dev <quocanhdev99@gmail.com>
2024-12-01 18:49:55 +0700

    feature(dev-blog): Add shortcut UI

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,24 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaPlus } from "react-icons/fa";
-import { HiAdjustmentsHorizontal } from "react-icons/hi2";
-import "./Blog.scss";
+import { TiDelete } from "react-icons/ti";
 import BlogListComponent from "../BlogList/BlogList";
+import "./Blog.scss";
 
 const BlogComponent = () => {
+  const [shortcutList, setShortcutList] = useState([]);
+
+  const addShortcut = () => {
+    const newShortCut = {
+      id: shortcutList.length + 1,
+      img: "https://th.bing.com/th/id/R.0a4bd0c65bd43b55d60bf1bc730aedb6?rik=xEd2WYn6TGoZTA&pid=ImgRaw&r=0",
+      text: "Title",
+    };
+
+    setShortcutList([...shortcutList, newShortCut]);
+  };
+
+  const removeShortcut = (idx) => {
+    setShortcutList((prevList) => prevList.filter((_, index) => index !== idx));
+  };
+
   return (
-    <div className="blog-wrapper">
-      {/* TODO: Will change another UI */}
-      <div className="blog-extension">
-        <button className="extension-btn">
-          <HiAdjustmentsHorizontal className="extension-icon" />
-          <span className="text">Feed settings</span>
-        </button>
-        <button className="extension-btn">
-          <FaPlus className="extension-icon" />
-          <span className="extension-text">Shortcuts</span>
-        </button>
+    <div className="blog">
+      <div className="blog__extension-list">
+        {shortcutList?.length > 0 &&
+          shortcutList.map((item, idx) => (
+            <div key={item?.id} className="blog__extension">
+              <button className="blog__extension-btn">
+                <img
+                  className="blog__extension-btn-img"
+                  src={item?.img}
+                  alt="logo-dark-mode"
+                />
+              </button>
+              <div className="blog__extension-btn-remove">
+                <TiDelete onClick={() => removeShortcut(idx)} />
+              </div>
+              <span className="blog__extension-btn-text">
+                {item?.text} - {idx}
+              </span>
+            </div>
+          ))}
+        {shortcutList?.length < 6 && (
+          <div className="blog__extension">
+            <button className="blog__extension-btn" onClick={addShortcut}>
+              <FaPlus className="blog__extension-btn-icon" />
+            </button>
+            <span className="blog__extension-btn-text">Shortcuts</span>
+          </div>
+        )}
       </div>
-      <BlogListComponent></BlogListComponent>
+      <BlogListComponent />
     </div>
   );
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,15 +14,15 @@ const HeaderComponent = () => {
   return (
     <header>
       <div className="header">
-        <div className="logo" onClick={() => handleClick("/")}>
+        <div className="header__logo" onClick={() => handleClick("/")}>
           <img
-            className="logo_img"
+            className="header__logo-img"
             src="/assets/images/logo-dark-mode.png"
             alt="logo-dark-mode"
           />
         </div>
-        <SearchComponent></SearchComponent>
-        <ProfileComponent></ProfileComponent>
+        <SearchComponent />
+        <ProfileComponent />
       </div>
     </header>
   );
